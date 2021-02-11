9f616e0a8fa063d64957e2b3166578e6593af40d
kiranmukherjee <77300690+kiranmukherjee@users.noreply.github.com>
2021-02-11 13:45:26 -0800

    Cannot figure out how to do this

---
+++ script.js
@@ -0,0 +1,15 @@
+var test = "test";
+    document.getElementById("input").value = test;
+
+
+let button1 = document.getElementById("inputbutton")
+
+submit.onclick = test()
+
+function test(){
+    values = ["dark","light","dusk","night"];
+    for (i=0; i<values.length; i++) {}
+    console.log("You chose " + test);
+}         
+
+
